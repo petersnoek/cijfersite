31fb90813e0ff1ddaba6e0f12dd8d05eaec84fc3
LAPTOP_DAAN\daank <daankraaijeveld@gmail.com>
2018-12-11 10:46:44 +0100

    Initial commit

---
+++ DVCToday_Admin/DVCToday_Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DVCToday_Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
